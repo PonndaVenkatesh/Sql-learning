81e407f38bf629fb03852901f46960e0e2007172
Venkatesh Ponnda <venkateshponnda@gmail.com>
2026-02-02 11:12:41 +0530

    Create Day 1_select.sql

---
+++ Day 1_select.sql
@@ -0,0 +1,12 @@
+-- Day 1: Basic SELECT statements
+
+-- Select all columns
+SELECT * FROM employees;
+
+-- Select specific columns
+SELECT name, department FROM employees;
+
+-- Limit output
+SELECT * FROM employees
+LIMIT 5;
+
